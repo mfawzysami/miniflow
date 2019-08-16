7d31eaa7f3aa0d8b4b6181d62ea7bb586f75a789
Mohamed Fawzy <mfawzy.sami@gmail.com>
2019-08-16 16:24:41 +0300

    Allowing reusing the same project folder

--- miniflow/project.py
+++ miniflow/project.py
@@ -9,30 +9,43 @@ class Project(object):
         self.data_dir = data_dir
         self.args = kwargs
         self.vars = {}
+        self.tasks = {}
+        self.counter = 1
+        self.prepared = False
 
     def prepare(self):
         if not os.path.exists(self.base_dir):
             os.mkdir(self.base_dir)
-        else:
-            raise Exception("Project directory already exists, you can't mix multiple workflows/pipelines together , aborting....")
-
+        tmp_dir = os.path.join(self.base_dir, "tmp")
+        logs_dir = os.path.join(tmp_dir, "logs")
+        tasks_dir = os.path.join(tmp_dir, "tasks")
         if not os.path.exists(os.path.join(self.base_dir, "tmp")):
-            tmp_dir = os.path.join(self.base_dir, "tmp")
-            logs_dir = os.path.join(tmp_dir, "logs")
-            tasks_dir = os.path.join(tmp_dir, "tasks")
             os.mkdir(tmp_dir)
             os.mkdir(logs_dir)
             os.mkdir(tasks_dir)
-            self.vars["base_dir"] = self.base_dir
-            self.vars["data_dir"] = self.data_dir
-            self.vars["tmp_dir"] = tmp_dir
-            self.vars["logs_dir"] = logs_dir
-            self.vars["tasks_dir"] = tasks_dir
+        self.vars["base_dir"] = self.base_dir
+        self.vars["data_dir"] = self.data_dir
+        self.vars["tmp_dir"] = tmp_dir
+        self.vars["logs_dir"] = logs_dir
+        self.vars["tasks_dir"] = tasks_dir
 
     def then(self, task):
-        self.prepare()
+        if not self.prepared:
+            self.prepare()
+            self.prepared = True
+
         next_task = task
         if not task:
             raise Exception("Task should be an instance of a task")
         next_task.project = self
-        return next_task
+        self.tasks[self.counter] = next_task
+        self.counter += 1
+        return self
+
+    def run(self):
+        print("Starting MiniFlow Pipeline")
+        for index in range(1,len(self.tasks.items())+1):
+           executing_task = self.tasks[index]
+           executing_task.run()
+        print ("Finished Pipeline")
+
--- miniflow/tasks.py
+++ miniflow/tasks.py
@@ -1,4 +1,3 @@
-
 from executor import execute
 from jinja2 import Template
 from .email_utils import send_email
@@ -11,9 +10,6 @@ def check_error(e):
         return str(e)
 
 
-
-
-
 class Task(object):
 
     def __init__(self, name, command, *positional, **kwargs):
@@ -23,22 +19,20 @@ class Task(object):
         self.args = kwargs
         self.project = None
 
+    def __preprocess_param__(self, param):
+        try:
+            if not param or not self.project:
+                return param
+            t = Template(param)
+            return t.render(**self.project.vars)
+        except Exception as e:
 
-    def __preprocess_param__(self,param):
-       try:
-           if not param or not self.project:
-               return param
-           t = Template(param)
-           return t.render(**self.project.vars)
-       except Exception as e:
-
-           return param
-
+            return param
 
     def prepare(self):
         import os
-        if self.project and not os.path.exists(os.path.join(self.project.base_dir,"tmp/tasks/{0}".format(self.name))):
-            task_dir = os.path.join(self.project.base_dir,"tmp/tasks/{0}".format(self.name))
+        if self.project and not os.path.exists(os.path.join(self.project.base_dir, "tmp/tasks/{0}".format(self.name))):
+            task_dir = os.path.join(self.project.base_dir, "tmp/tasks/{0}".format(self.name))
             self.self_dir = task_dir
             os.mkdir(task_dir)
             if self.project:
@@ -46,16 +40,18 @@ class Task(object):
 
     def __get_params__(self):
         if len(self.args.items()) > 0:
-            params = ["--{0}={1}".format(k, self.__preprocess_param__(v)) for k, v in self.args.items() if "mail_" not in k]
+            params = ["--{0}={1}".format(k, self.__preprocess_param__(v)) for k, v in self.args.items() if
+                      "mail_" not in k]
             return " ".join(params)
         else:
             return ""
 
-    def logit(self,output):
+    def logit(self, output):
         import os
         try:
-            if self.project and os.path.exists(os.path.join(self.project.base_dir,"tmp/logs")):
-                with open(os.path.join(self.project.base_dir,"tmp/logs/{0}_logs.txt".format(self.name)),mode='w+') as writer:
+            if self.project and os.path.exists(os.path.join(self.project.base_dir, "tmp/logs")):
+                with open(os.path.join(self.project.base_dir, "tmp/logs/{0}_logs.txt".format(self.name)),
+                          mode='w+') as writer:
                     writer.write(output)
 
         except Exception as e:
@@ -74,13 +70,13 @@ class Task(object):
                 preprocessed_positionals = [self.__preprocess_param__(param) for param in self.positional]
                 tool_command += " " + " ".join(preprocessed_positionals)
 
-            full_command = "{0} {1}".format(tool_command,self.__get_params__())
+            full_command = "{0} {1}".format(tool_command, self.__get_params__())
 
             # now execute the current task
             print("Executing : {0}".format(full_command))
-            logs = execute(full_command,capture=True)
+            logs = execute(full_command, capture=True)
             self.logit(logs)
-            if self.args.get('notify',False):
+            if self.args.get('notify', False):
                 self.notify(self.args.get('mail_receiver', None), self.args.get('mail_message', None))
 
             return self
@@ -107,12 +103,10 @@ class Task(object):
         except Exception as e:
             print(check_error(e))
 
-
-
     def notify(self, email=None, message=None):
         try:
             if self.project:
-                send_email(self.project.args,mail_message=message)
+                send_email(self.project.args, mail_message=message)
         except Exception as e:
             msg = check_error(e)
-            print (msg)
+            print(msg)
