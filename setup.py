6b0a2cf487d512ca686b375a546bb5c8e06e602b
Mohamed Fawzy <mfawzy.sami@gmail.com>
2020-06-29 19:42:59 +0300

    Converting miniflow into an egg

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def read(name):
+    with open(name, 'r') as input:
+        return input.read()
+
+
+def requirements():
+    with open('requirements.txt', 'r') as req:
+        return req.readlines()
+
+
+setup(
+    name="Miniflow Python Package",
+    version="0.0.5",
+    author="Mohamed Fawzy",
+    author_email="mfawzy.sami@gmail.com",
+    description="Official Python Package for Mini Bioinformatics workflow wrapper (Miniflow)",
+    license="BSD",
+    keywords="miniflow",
+    url="https://github.com/mfawzysami/miniflow",
+    packages=["miniflow"],
+    install_requires=requirements(),
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
